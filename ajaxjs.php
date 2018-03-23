eb8824900379db6bf895986cbc5a4508ca356103
Raymond Yeh <yehray@gmail.com>
2018-03-22 18:36:18 -0700

    added add and delete data methods

--- ajaxjs.php
+++ ajaxjs.php
@@ -2,10 +2,21 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+$date2 = $_POST['date1'];
 $calories2 = $_POST['calories1'];
 $weight2 = $_POST['weight1'];
+$action = $_POST['action1'];
 $connection = mysqli_connect("localhost", "root", "7UPdrinker", "fitness_database") or die("Unable to connect to MySQL");  
-$query = mysqli_query($connection, "insert into FitnessData(calories, weight) values ('$calories2', '$weight2')");
+if ($action ==  "add") {
+    $query = mysqli_query($connection, "INSERT INTO FitnessData(Dates, Calories, Weight) VALUES ('$date2', '$calories2', '$weight2')");
+}
+if ($action ==  "delete") {
+    echo $date2;
+    $query = mysqli_query($connection, "DELETE FROM FitnessData WHERE Dates = '$date2'");
+}
+
+echo mysqli_errno($connection) . ": " . mysqli_error($connection) . "\n";
 echo "Form Submitted succesfully";
 mysqli_close($connection);
 ?>
+
--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-function myFunction(){
+function addData(){
+    var date = new Date(document.getElementById("datepicker").value).toISOString();
     var calories = document.getElementById("calories").value;
     var weight = document.getElementById("weight").value;
-    var dataString = 'calories1=' + calories + '&weight1=' + weight;
-    if (calories == '' || weight == ''){
+    var dataString = 'date1=' + date.slice(0,10) + '&calories1=' + calories + '&weight1=' + weight + '&action1=' + 'add';
+    console.log(date);
+    console.log(dataString);
+    if (date == '' || calories == '' || weight == ''){
         window.alert("Please Fill All Fields");
+    }
+    else{
+        $.ajax({
+            type: "POST",
+            url: "ajaxjs.php",
+            data: dataString,
+            cache: false,
+            crossDomain : true,
+            success: function() {
+            }
+        });
+    }
+    return false;
+}
+
+function deleteData(){
+    var date = new Date(document.getElementById("datepicker").value).toISOString();
+    var dataString = 'date1=' + date.slice(0,10) + '&action1=' + 'delete';
+    if (date == ''){
+        window.alert("Please Fill a Date");
     }
     else{
         $.ajax({
