91ee74d877d8557b46f6e70b22f6b3ea0f9eca86
Raymond Yeh <yehray@gmail.com>
2018-04-23 20:39:16 -0700

    added formatting for plots

--- python/graphs.py
+++ python/graphs.py
@@ -14,25 +14,34 @@ df = pd.read_sql("SELECT * FROM DailyFoodData WHERE Username = '" + sys.argv[1] 
 
 # Group by dates
 weight_df = df.groupby('Dates', as_index=False)["Weight"].mean()
-weight_df.time = pd.to_datetime(weight_df['Dates'], format='%Y-%m-%d')
-weight_df.set_index(['Dates'],inplace=True)
-weight_df.plot.line()
-plt.title("Weights")
-plt.xlabel("Dates")
-plt.ylabel("Weight")
+# weight_df.time = pd.to_datetime(weight_df['Dates'], format='%Y-%m-%d')
+
+changed_df = df.groupby('Dates', as_index=False)["Weight"].mean()
+changed_df['Weight']  = changed_df['Weight'] - changed_df['Weight'].shift(-1)
+changed_df.iloc[-1,-1]= 0
+
+fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(9,9))
+fig.subplots_adjust(hspace=.5)
+
+ax1 = weight_df.plot(ax=axes[0])
+ax1.title.set_text('Weights')
+ax1.set_xlabel('Days')
+ax1.set_ylabel('Weight')
+ax1.patch.set_facecolor('#f2f2f2')
+
+ax2 = changed_df.plot(ax=axes[1], color=['red'])
+ax2.title.set_text('Change in Weight')
+ax2.set_xlabel('Days')
+ax2.set_ylabel('Change in Weight')
+ax2.legend(['Change in Weight'])
+ax2.patch.set_facecolor('#f2f2f2')
+
+
 timestr = time.strftime("%Y%m%d-%H%M%S")
-plt.savefig("/Users/yehray/Sites/fitness_tracker/python/plots/" + str(sys.argv[1]) + timestr + ".png")
+plt.savefig("/Users/yehray/Sites/fitness_tracker/python/plots/" + str(sys.argv[1]) + timestr + ".png", facecolor='#f2f2f2')
+
 print("python/plots/" + str(sys.argv[1]) + timestr +  ".png")
 
-# changed_df = weight_df
 
-# changed_df['Weight']  = changed_df['Weight'] - changed_df['Weight'].shift(-1)
-# weight_df.iloc[-1,-1]= 0
 
-# weight_df.plot.line()
-# plt.title("Weights")
-# plt.xlabel("Dates")
-# plt.ylabel("Weight")
-# plt.xticks(rotation=70)
-# plt.show()
 
